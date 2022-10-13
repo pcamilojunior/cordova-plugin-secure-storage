bfe964e845a027d4b7e4e161904277d2e4fe467e
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-10-13 13:41:58 +0100

    feat: add hook to copy migration authentication value to strings.xml
    As the default value for this property is false, we only change it if it is exactly equal to true.
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-1846

---
+++ hooks/android/androidCopyPreferences.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+const { ConfigParser } = require('cordova-common');
+
+module.exports = function (context) {
+    var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
+    var configXML = path.join(projectRoot, 'config.xml');
+    var configParser = new ConfigParser(configXML);
+    var authenticate = configParser.getGlobalPreference("MigratedValuesAuthentication");
+
+    if(authenticate === true){
+        var stringsXmlPath = path.join(projectRoot, 'platforms/android/app/src/main/res/values/strings.xml');
+        var stringsXmlContents = fs.readFileSync(stringsXmlPath).toString();
+        var etreeStrings = et.parse(stringsXmlContents);
+
+        var migrationAuthTags = etreeStrings.findall('./string[@name="migration_auth"]');
+        for (var i = 0; i < migrationAuthTags.length; i++) {
+            migrationAuthTags[i].text = authenticate;
+        }
+
+        var resultXmlStrings = etreeStrings.write();
+        fs.writeFileSync(stringsXmlPath, resultXmlStrings);
+    }
+};
